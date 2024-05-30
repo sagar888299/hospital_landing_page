0ee00944de26f8d64f7ffe6a46ea09610a3789b7
Sagar <sagar888299@gmail.com>
2024-05-30 21:02:36 +0530

    init

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import { graphql, useStaticQuery } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import Navbar from "../Components/navbar";
+import Navbar from "../Components/Navbar";
 import Section1 from "../Components/Section1";
 import Section2 from "../Components/Section2";
 import Section3 from "../Components/Section3";
 import Section4 from "../Components/Section4";
 import Section6 from "../Components/Section6";
 import Section5 from "../Components/Section5";
-import Footer from "../Components/footer";
-
+import Footer from "../Components/Footer";
 import '../Styles/style.css';  
 
 const IndexPage = () => {
